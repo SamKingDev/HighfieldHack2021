78645ffed5863ddf64c4077d366d49b62a41b23c
unknown <emma12812@gmail.com>
2021-02-13 00:10:07 +0000

    Working on profile

--- App/lib/screens/profile/profile.dart
+++ App/lib/screens/profile/profile.dart
@@ -21,7 +21,28 @@ class _ProfileState extends State<Profile> {
                       Colors.blue
                     ])
                 ),
-                child: Text('This is a drawer header')),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Material(
+                        borderRadius: BorderRadius.all(Radius.circular(60.0)),
+                        elevation: 10,
+                        child: Padding(padding: EdgeInsets.all(8.0),
+                        child: Image.asset('assets/logo.png', width: 80, height: 80),
+                      ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Famealy Planner',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),),
+                      ),
+                    ],
+                  ),
+                ),
+            ),
             CustomListTile(Icons.person, 'Profile', ()=>{}),
             CustomListTile(Icons.notifications, 'Notifications', ()=>{}),
             CustomListTile(Icons.settings, 'Settings', ()=>{}),
@@ -36,71 +57,51 @@ class _ProfileState extends State<Profile> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
+        padding: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/logo.png'),
-                radius: 70.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text (
-                  'First Name:',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                Text ('...',
-                style: TextStyle(
-                  fontSize: 25,
-                ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text (
-                  'Last Name:',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                Text (
-                    '...',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text (
-                  'Family Group:',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                Text (
-                  '...',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
+            Image(image: AssetImage('assets/logo.png')),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget> [
+                CustomRow('First Name', '...'),
+                CustomRow('Last Name', '...'),
+                CustomRow('Family Group', '...'),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomRow extends StatelessWidget {
+
+  String text;
+  String content;
+
+  CustomRow(this.text, this.content);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        Text(
+          content,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        SizedBox(height: 30.0),
+      ],
     );
   }
 }
@@ -119,31 +120,36 @@ class CustomListTile extends StatelessWidget{
     // TODO: implement build
     return Padding(
       padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-      child: InkWell(
-        splashColor: Colors.lightBlueAccent,
-        onTap: onTap,
-        child: Container(
-          height: 40,
-          child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, //x axis
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Icon(icon),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(text,
-                  style: TextStyle(
-                    fontSize: 16.0
-                  ),
-                  ),
-                ),
-              ],
-            ),
-            Icon(Icons.arrow_right),
-          ],
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey[400]))
         ),
-      ),
+        child: InkWell(
+          splashColor: Colors.lightBlueAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, //x axis
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(icon),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(text,
+                    style: TextStyle(
+                      fontSize: 16.0
+                    ),
+                    ),
+                  ),
+                ],
+              ),
+              Icon(Icons.arrow_right),
+            ],
+          ),
+        ),
+        ),
       ),
     );
   }
