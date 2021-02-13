490d789eac3b9a8eb1ec663152532c39bc8c5f89
Sam King <smadger@live.co.uk>
2021-02-13 01:10:22 +0000

    Firebase Auth added

---
+++ App/lib/blocs/auth_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:famealy/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthBloc {
+  final authService = AuthService();
+  final googleSignIn = GoogleSignIn(scopes: ['email']);
+
+  Stream<User> get currentUser => authService.curentUser;
+
+  loginGoogle() async {
+    try {
+      final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken
+      );
+
+      final result = await authService.signInWithCredential(credential);
+
+      print('${result.user.displayName}');
+    } catch (e){
+      print(e);
+    }
+  }
+}
