6548aa27433d42d0febcc4bba76f75e134ab481b
Sam King <smadger@live.co.uk>
2021-02-13 14:24:58 +0000

    Profile page scrollable

--- App/lib/screens/Meal/MealPlan.dart
+++ App/lib/screens/Meal/MealPlan.dart
@@ -8,36 +8,107 @@ class mealPlan extends StatefulWidget {
 class _mealPlanState extends State<mealPlan> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [
-        Container(
-          child: Material(
-            elevation: 20,
-            child: Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Image.asset('assets/logo.png'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose Week'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            child: Material(
+              elevation: 20,
+              child: Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Image.asset('assets/logo.png'),
+              ),
+            ),
+          ),
+          Container(
+              child: DateListTile('date1', (){})
+          ),
+          Container(
+              child: DateListTile('date2', (){})
+          ),
+          Container(
+              child: DateListTile('date3', (){})
+          ),
+          Container(
+              child: DateListTile('date4', (){})
+          ),
+          Container(
+              child: DateListTile('date5', (){})
+          ),
+          Container(
+              child: DateListTile('date6', (){})
+          ),
+          Container(
+              child: DateListTile('date6', (){})
+          ),
+          Container(
+              child: DateListTile('date6', (){})
+          ),
+          Container(
+              child: DateListTile('date6', (){})
+          ),Container(
+              child: DateListTile('date6', (){})
+          ),
+          Container(
+              child: DateListTile('date6', (){})
+          ),
+
+        ]),
+      ),
+    );
+  }
+}
+
+
+class DateListTile extends StatefulWidget {
+  String date;
+  Function onTap;
+
+  DateListTile(this.date, this.onTap);
+
+  @override
+  _DateListTile createState() => _DateListTile();
+}
+
+class _DateListTile extends State<DateListTile> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey[400]))),
+        child: InkWell(
+          splashColor: Colors.lightBlueAccent,
+          onTap: widget.onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, //x axis
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(widget.date,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      )
+                      ),
+                      ),
+                  ],
+                ),
+                Icon(Icons.arrow_right),
+              ],
             ),
           ),
         ),
-        Container(
-
-        ),
-        Container(
-
-        ),
-        Container(
-
-        ),
-        Container(
-
-        ),
-        Container(
-
-        ),
-        Container(
-
-        ),
-      ]),
+      ),
     );
   }
 }
